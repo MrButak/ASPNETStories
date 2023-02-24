30754d3d2cf37da0bd95444d94dc063c75824e2f
mrbutak <mspence5555@gmail.com>
2023-02-24 08:52:06 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stories.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
