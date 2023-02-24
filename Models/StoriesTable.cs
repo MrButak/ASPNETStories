4cf68aabe97293efef485c2021079db81ca572bb
mrbutak <mspence5555@gmail.com>
2023-02-24 09:22:32 -0600

    Created Models for Stories and Paragraphs

---
+++ Models/StoriesTable.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stories.Models
+{
+    public class StoriesTable
+    {
+        public int StoryId { get; set; }
+
+        public string StoryTitle { get; set; } = null!;
+
+        [DataType(DataType.DateTime)] public DateTime StoryCreatedAt { get; set; } = DateTime.Now;
+        [DataType(DataType.DateTime)] public DateTime StoryUpdatedAt { get; set; } = DateTime.Now;
+
+        // One story to many paragraphs
+        public List<ParagraphsTable> ParagraphsTable { get; set; } = null!;
+    }
+}
