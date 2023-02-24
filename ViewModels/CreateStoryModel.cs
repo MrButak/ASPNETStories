dd01389e51334f9fa32928c036824881b4ae66c4
mrbutak <mspence5555@gmail.com>
2023-02-24 13:33:48 -0600

    Created View Model for user to create story

---
+++ ViewModels/CreateStoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stories.ViewModels
+{
+    public class CreateStoryModel
+    {
+        [Required(ErrorMessage = "Please give your story a title")]
+        [Display(Name = "Story Title")]
+        [MaxLength(255)]
+        public string StoryTitle { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please give your story at least one paragraph")]
+        [Display(Name = "Story Text")]
+        public string ParagraphText { get; set; } = null!;
+    }
+}
